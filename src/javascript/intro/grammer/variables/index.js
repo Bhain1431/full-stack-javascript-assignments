45785e98d875877ed05e71ad65d252853ce85b8c
Brad Hain <bradhain@gmail.com>
2017-01-24 16:29:07 -0700

    mess up change

--- src/javascript/intro/grammer/variables/index.js
+++ src/javascript/intro/grammer/variables/index.js
@@ -1,10 +1,21 @@
 
-
+//1
 export var message = 'hello';
 
 export let greeting = 'Yo!';
 
 export const saying = 'what up';
+
+console.log(hello);
+console.log(yo);
+console.log(whatup);
+
+//2
+export var message = 'world';
+//3
+export let greeting = 'hello';
+//4
+//can't rename a const.
 
 
 
