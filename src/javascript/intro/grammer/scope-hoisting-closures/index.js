1a271de69ac66f9a026ab81de6dc266a1b52b457
Brad Hain <bradhain@gmail.com>
2017-02-01 18:58:02 -0700

    scope hosting work

--- src/javascript/intro/grammer/scope-hoisting-closures/index.js
+++ src/javascript/intro/grammer/scope-hoisting-closures/index.js
@@ -1,19 +1,22 @@
 export const ssn = value => {
 
-  const sanitize = () => value.replace(/\D+/g, '');
+  const sanitize = () => value.replace(/\D+/g, '');// This RegEx removes non-digit charictures such asnletters, spaces ,and puctuation
 
-  const format = (value) => `${value.substring(0, 3)}-${value.substring(3, 5)}-${value.substring(5)}`;
+  const format = (value) => `${value.substring(0, 3)}-${value.substring(3, 5)}-${value.substring(5)}`;// This one formats the number to look like this 000-00-0000
+
+  const sanitized = sanitize();
 
   const isValid = () => {
-    // Your code goes here....
-    //  return ??
-  };
 
-  return {
+  return sanitized.length === 10
+  };
+  return{
+
 
     isValid,
 
     toString(){
+      return format(sanitized)
 
     }
   };
@@ -21,8 +24,8 @@ export const ssn = value => {
 
 
 
-// Why does this work?
-export const result = calculate(1,2);
+// Why does this work?.......  This works because of hoisting even though line 28 dosent know what calculate is because it is given a value later Javascript mooves it up.
+export const result = calculate(1,2);         //but this is stupid dont do it.
 
 function calculate(x, y) {
   return x + y;
