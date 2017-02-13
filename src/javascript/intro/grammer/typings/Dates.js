f783ddc0327ff2f088f5cda3c29f3332ad02a7da
Brad Hain <bradhain@gmail.com>
2017-02-12 21:24:02 -0700

    Home work on math, numbe,r and date

--- src/javascript/intro/grammer/typings/Dates.js
+++ src/javascript/intro/grammer/typings/Dates.js
@@ -5,3 +5,22 @@ new Date('02022017');
 console.log('02022017');
 
 
+/*1. create and format Dates and parse them from strings
+  2. use the built-in Number object's properties and methods
+  3. use the built-in Math object's properties and methods*/
+//#1
+const today = new Date('2017-02-12T03:30:00');
+
+date.parse('1982-02-12T03:30:00');
+//#2
+const biggestNum = Number.MAX_VALUE; //shows the max value to a numeric variable.
+const  b = new Date('0000-00-00'); //converets date object to a numeric value.
+Number('000000' ); //converts number string to number.
+//#3
+math.PI//is a math property that gives you pie.
+Math.round(x); //round to the nearest intager it is a method.
+
+
+
+
+
